000f058cc528d62769b9a54a2d8ffaa04957aabc
guy <guy_wichapad@hotmail.com>
2023-06-20 08:53:57 +0700

    swiper

--- client/src/Components/HomeComponents/slides.js
+++ client/src/Components/HomeComponents/slides.js
@@ -1,56 +1,69 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectCube, Pagination } from "swiper";
 import "swiper/css";
-import '../../App.css'
 import "swiper/css/effect-cube";
 import "swiper/css/pagination";
 
-
 const Slide = () => {
-
   return (
-    <div className="slide-bg-image">
-      <div className="flex ">
-        <Swiper
-          effect={"cube"}
-          grabCursor={true}
-          cubeEffect={{
-            shadow: true,
-            slideShadows: true,
-            shadowOffset: 20,
-            shadowScale: 0.94,
-          }}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          pagination={{ clickable: true }}
-          modules={[Autoplay, EffectCube, Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <img src="https://pbs.twimg.com/media/FoIK24XXkAEODKj.jpg" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://pbs.twimg.com/media/FiIGvfqaEAA6z8Z.jpg" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://pbs.twimg.com/media/C2DFgzrXEAE7Gu1.jpg" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://pbs.twimg.com/media/CVcmkLbUkAEHxpJ.jpg" alt="" />
-          </SwiperSlide>
-        </Swiper>
-
-        <div className="">
-          <img
-            className="w-32"
-            src="https://pbs.twimg.com/media/FoIK24XXkAEODKj.jpg"
-            alt=""
-          />
+    <>
+      <div className="slide-bg-image">
+        <div className="absolute top-6 left-4">
+          <Swiper
+            effect={"cube"}
+            grabCursor={true}
+            cubeEffect={{
+              shadow: true,
+              slideShadows: true,
+              shadowOffset: 20,
+              shadowScale: 0.94,
+            }}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            pagination={{ clickable: true }}
+            modules={[Autoplay, EffectCube, Pagination]}
+            className="mySwiper swiper-container"
+          >
+            <SwiperSlide>
+              <img
+                src="https://pbs.twimg.com/media/FoIK24XXkAEODKj.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="https://pbs.twimg.com/media/FiIGvfqaEAA6z8Z.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="https://pbs.twimg.com/media/C2DFgzrXEAE7Gu1.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="https://pbs.twimg.com/media/CVcmkLbUkAEHxpJ.jpg"
+                alt=""
+              />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
-    </div>
+      <div className="w-60 flex">
+        <div className="w-full">
+          <img src="https://pbs.twimg.com/media/FoIK24XXkAEODKj.jpg" alt="" />
+        </div>
+        <div>
+          <span>New</span>
+          <p>Bring Me The Horizon</p>
+          <span>59.99 $</span>
+        </div>
+      </div>
+    </>
   );
 };
 
