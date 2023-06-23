39333e2b62e2c0c2931287c6335c64a32fe16cbd
guy <guy_wichapad@hotmail.com>
2023-06-23 08:55:30 +0700

    edit News

--- client/src/Components/HomeComponents/Home.js
+++ client/src/Components/HomeComponents/Home.js
@@ -5,18 +5,20 @@ import News from "./News";
 
 const Home = () => {
   return (
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto ">
-      <div className="home-background">
-        <img
-          className=" home-background-img "
-          src="https://images.unsplash.com/photo-1577648875929-894904f7b051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-          alt="bg-home"
-        />
+    <>
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto ">
+        <div className="home-background">
+          <img
+            className=" home-background-img "
+            src="https://images.unsplash.com/photo-1577648875929-894904f7b051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+            alt="bg-home"
+          />
+        </div>
       </div>
       <Card />
       <Slide />
       <News />
-    </div>
+    </>
   );
 };
 
--- client/src/Components/HomeComponents/News.js
+++ client/src/Components/HomeComponents/News.js
@@ -1,106 +1,84 @@
 const News = () => {
   return (
-    <div className="w-full my-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 gap-4  my-8 mx-16">
+      <div className="border-b-2 border-black w-full">
+      <span className="text-2xl">Showroom News</span>
+      </div>
       {/* item 1 */}
-      <div className="flex shadow-lg mx-8 rounded">
-        <div className="w-60">
+      <div className="relative flex shadow-lg mx-2  md:col-span-2 lg:col-span-3 rounded">
+        <div className="w-96 md:w-60 lg:w-60">
           <img
-            className="w-full h-full object-cover rounded"
+            className=" w-full h-full object-cover rounded"
             src="https://pbs.twimg.com/media/FoIK24XXkAEODKj.jpg"
             alt=""
           />
         </div>
-        <div className="p-4 ">
-          <span className="bg-red-600 rounded p-1 text-white">News</span>
-          <p className="my-2 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </p>
-          <p className="text-gray-500 ">
+        <div className="p-4 text-sm">
+          <span className="bg-red-600 rounded p-1 text-base text-white">
+            News
+          </span>
+
+          <p className="text-gray-500 my-2">
             Lorem Ipsum has been the industry's standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book.
+            the 1500s, when an unknown.Lorem Ipsum
           </p>
-          
-          <div className=" mt-16 ">
-            <span className="text-sm font-semibold underline cursor-pointer">
+
+          <div className="absolute bottom-3">
+            <span className=" font-semibold underline cursor-pointer">
               See Detail
             </span>
           </div>
         </div>
       </div>
 
-      {/* <div className="flex flex-col"> */}
-        {/* item 2 */}
-        {/* <div className="flex shadow-lg my-2 mx-2 rounded">
-          <div className="w-60">
-            <img
-              className="w-full h-full object-cover rounded"
-              src="https://pbs.twimg.com/media/FoIK24XXkAEODKj.jpg"
-              alt=""
-            />
-          </div>
-          <div className="p-4 ">
-            <span className="bg-red-600 rounded p-1 text-white">News</span>
-            <p className="my-2 text-sm">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-
-            <div className=" mt-16 ">
-              <span className="text-sm font-semibold underline cursor-pointer">
-                See Detail
-              </span>
-            </div>
-          </div>
-        </div> */}
-        {/* item 3 */}
-        {/* <div className="flex  shadow-lg my-2 mx-2 rounded">
-          <div className="w-60">
-            <img
-              className="w-full h-full object-cover rounded"
-              src="https://pbs.twimg.com/media/FoIK24XXkAEODKj.jpg"
-              alt=""
-            />
-          </div>
-          <div className="p-4">
-            <span className="bg-red-600 rounded p-1 text-white">News</span>
-            <p className="my-2 text-sm">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-
-            <div className=" mt-16">
-              <span className="text-sm font-semibold underline cursor-pointer">
-                See Detail
-              </span>
-            </div>
-          </div>
-        </div> */}
-        {/* item 4 */}
-        {/* <div className="flex shadow-lg my-2 mx-2   rounded">
-          <div className="w-60">
-            <img
-              className="w-full h-full object-cover rounded"
-              src="https://pbs.twimg.com/media/FoIK24XXkAEODKj.jpg"
-              alt=""
-            />
-          </div>
-          <div className="p-4">
-            <span className="bg-red-600 rounded p-1 text-white">News</span>
-            <p className="my-2 text-sm">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-
-            <div className=" mt-16">
-              <span className="text-sm font-semibold underline cursor-pointer">
-                See Detail
-              </span>
-            </div>
-          </div>
-        </div> */}
-      {/* </div> */}
+      {/* item 2 */}
+      <div className="relative flex lg:flex-col lg:w-60 shadow-lg mx-2 md:col-span-2 lg:col-span-1  rounded">
+        <div className="w-96 md:w-60">
+          <img
+            className=" w-full h-full object-cover rounded"
+            src="https://pbs.twimg.com/media/FoIK24XXkAEODKj.jpg"
+            alt=""
+          />
+        </div>
+        <div className="p-4 text-sm">
+          <p className="text-gray-500 my-2">
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown.Lorem Ipsum
+          </p>
+        </div>
+      </div>
+      {/* item 3 */}
+      <div className="relative flex lg:flex-col lg:w-60 shadow-lg mx-2 md:col-span-2 lg:col-span-1  rounded">
+        <div className="w-96 md:w-60 ">
+          <img
+            className=" w-full h-full object-cover rounded"
+            src="https://pbs.twimg.com/media/FoIK24XXkAEODKj.jpg"
+            alt=""
+          />
+        </div>
+        <div className="p-4 text-sm">
+          <p className="text-gray-500 my-2">
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown.Lorem Ipsum
+          </p>
+        </div>
+      </div>
+      {/* item 4 */}
+      <div className="relative flex lg:flex-col lg:w-60 shadow-lg mx-2 md:col-span-2 lg:col-span-1  rounded">
+        <div className="w-96 md:w-60">
+          <img
+            className=" w-full h-full object-cover rounded"
+            src="https://pbs.twimg.com/media/FoIK24XXkAEODKj.jpg"
+            alt=""
+          />
+        </div>
+        <div className="p-4 text-sm">
+          <p className="text-gray-500 my-2">
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown.Lorem Ipsum
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
