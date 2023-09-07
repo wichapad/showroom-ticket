9ccb24ac31a80ed34b8ed10d0c5a2024d8722e6c
guy <guy_wichapad@hotmail.com>
2023-09-07 21:06:53 +0700

    update artistControll

--- Back-end/routes/artistSchedule.js
+++ Back-end/routes/artistSchedule.js
@@ -1,10 +1,10 @@
 // Route of artstisSchedule controller
 const express = require("express");
-const { getartistSchedule } = require("../controllers/artistScheduleController");
+const { getartistSchedule, getartistScheduleByslug } = require("../controllers/artistScheduleController");
 
 const router = express.Router();
 
 router.get("/tour", getartistSchedule);
-router.get("/tour/:slug", getartistSchedule);
+router.get("/tour/:slug", getartistScheduleByslug);
 
 module.exports = router;
