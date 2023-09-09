3a41e14e5a032dc4b10c6ef401230e3454dbc8f9
guy <guy_wichapad@hotmail.com>
2023-09-09 17:26:56 +0700

    post api artist component

--- Front-end/src/Components/page/Dashboard/Content/Management/AristsManage/CreateArtists.jsx
+++ Front-end/src/Components/page/Dashboard/Content/Management/AristsManage/CreateArtists.jsx
@@ -1,11 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { HiX } from "react-icons/hi";
 import { ApiContext } from "../../../../../UseContext/ApiContext";
+import axios from "axios";
 
 const CreateArtists = ({ isVisible, handleCreate }) => {
   const { genreList } = useContext(ApiContext);
   const [closeCreate, setCloseCreate] = useState(false);
+  const [artistsForm, setArtistsForm] = useState({
+    artist_name: "",
+    genre_id: "",
+    artist_image: "",
+  });
+
+  const inputValueArtists = (e) => {
+    const { name, value } = e.target;
+    setArtistsForm({
+      ...artistsForm,
+      [name]: value,
+    });
+  };
+
+  //Create artists
+  const createArtists = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_API}/api/artists/create`,
+        artistsForm
+      );
+      setArtistsForm({
+        artist_name: "",
+        genre_id: "",
+        artist_image: "",
+      });
+    } catch (error) {
+      console.error("Error creating artist", error);
+    }
+  };
 
   //close Add event page
   const toggleClose = () => {
@@ -29,42 +61,59 @@ const CreateArtists = ({ isVisible, handleCreate }) => {
             <HiX />
           </div>
           <h1 className="text-center uppercase">Create artists</h1>
-          <div>
-            <label>Artist</label>
-            <input
-              type="text"
-              className="border border-gray-300 rounded p-[0.35rem]  focus:border-gray-700 outline-none appearance-none "
-            />
-          </div>
-          <div className="my-2">
-            <label>Genre</label>
-            <select
-              type="text"
-              className="w-full border border-gray-300 rounded p-[0.35rem] focus:border-gray-700 outline-none appearance-none "
-            >
-              <option>Select genre</option>
-              {genreList.map((genre) => (
-                <option key={genre.genre_id} value={genre.genre_id}>
-                  {genre.genre_name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label>Arist Image</label>
-            <input
-              type="text"
-              className="border border-gray-300 rounded p-[0.35rem]  focus:border-gray-700 outline-none appearance-none "
-            />
-          </div>
-          <div className="flex justify-evenly mt-2">
-            <button className="px-4 py-3 rounded-lg bg-blue-700 text-white hover:bg-blue-800">
-              Create
-            </button>
-            <button className="px-4 py-3 rounded-lg bg-red-700 text-white hover:bg-red-800">
-              Cancel
-            </button>
-          </div>
+          <form onSubmit={createArtists}>
+            <div>
+              <label>Artist</label>
+              <input
+                className="border border-gray-300 rounded p-[0.35rem]  focus:border-gray-700 outline-none appearance-none "
+                type="text"
+                name="artist_name"
+                value={artistsForm.artist_name}
+                onChange={inputValueArtists}
+              />
+            </div>
+            <div className="my-2">
+              <label>Genre</label>
+              <select
+                className="w-full border border-gray-300 rounded p-[0.35rem] focus:border-gray-700 outline-none appearance-none "
+                type="text"
+                name="genre_id"
+                value={artistsForm.genre_id}
+                onChange={inputValueArtists}
+              >
+                <option>Select genre</option>
+                {genreList.map((genre) => (
+                  <option key={genre.genre_id} value={genre.genre_id}>
+                    {genre.genre_name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label>Arist Image</label>
+              <input
+                className="border border-gray-300 rounded p-[0.35rem]  focus:border-gray-700 outline-none appearance-none "
+                type="text"
+                name="artist_image"
+                value={artistsForm.artist_image}
+                onChange={inputValueArtists}
+              />
+            </div>
+            <div className="flex justify-evenly mt-2">
+              <button
+                type="submit"
+                className="px-4 py-3 rounded-lg bg-blue-700 text-white hover:bg-blue-800"
+              >
+                Create
+              </button>
+              <button
+                onClick={toggleClose}
+                className="px-4 py-3 rounded-lg bg-red-700 text-white hover:bg-red-800"
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
