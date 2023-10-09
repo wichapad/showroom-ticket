12ee05e6ac3d9d3a9cddb280b4d043db61ad5d95
Wichapad Muenjumrad <84759066+wichapad@users.noreply.github.com>
2023-10-09 16:59:38 +0700

    Update ticketController.js

--- Back-end/controllers/ticketController.js
+++ Back-end/controllers/ticketController.js
@@ -77,9 +77,9 @@ JOIN
 JOIN 
   artist a ON a.artist_id = e.artist_id
 WHERE 
-  e.slug_event = $1
+  ctgy.ticket_ctgy_id = $1
 AND 
-ctgy.ticket_ctgy_id = $2
+  e.slug_event = $2
 GROUP BY 
   e.event_id,a.artist_name,e.event_date, e.event_time, ctgy.ticket_ctgy_id, ctgy.price;
 
