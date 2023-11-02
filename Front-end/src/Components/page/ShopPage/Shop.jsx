7908a75dd18dfe8fbf5819eb6e381180e3fec188
guy <guy_wichapad@hotmail.com>
2023-11-02 16:22:30 +0700

    update shop

--- Front-end/src/Components/page/ShopPage/Shop.jsx
+++ Front-end/src/Components/page/ShopPage/Shop.jsx
@@ -2,96 +2,78 @@ import React from "react";
 
 const shop = () => {
   return (
-    <div className="pt-[5rem] pb-[2rem] m-auto max-w-screen-2xl">
-      <div className="p-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <div className=" rounded-lg shadow bg-gray-800 ">
-          <img
-            className="rounded w-full object-cover"
+    <div className="pt-[5rem] pb-[2rem] px-4">
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 max-w-screen-2xl mx-auto">
+        <div className="shop-container">
+          <div>
+            <img
+            className="shop-image"
             src="https://upload.wikimedia.org/wikipedia/en/b/b0/Yoasobi_-_Idol.png"
             alt=""
           />
-          <div className="p-5 text-white">
-            <p className="text-gray-500">Albums</p>
+          </div>
+          <div className="shop-text">
             <h1>Yoasobi</h1>
-            <p className="py-2">Album: Yoru ni Kakeru</p>
-            <p>29.99 $</p>
-            <div className="pt-2 text-center">
-              <button className="w-full uppercase text-sm font-bold py-3 px-4 text-gray-200 rounded-md bg-gradient-to-r from-purple-600 via-violet-700 to-purple-600 active:scale-95">
-                Buy now
-              </button>
-            </div>
+            <p>Album: Yoru ni Kakeru</p>
+          </div>
+          <div className="shop-price">
+            <p>$29.99 </p>
           </div>
         </div>
-        <div className="   rounded-lg shadow bg-gray-800">
+        <div className="shop-container">
           <img
-            className=" rounded w-full object-cover"
+            className="shop-image"
             src="https://shopeu.hstyles.co.uk/cdn/shop/products/whiterabbit.png?v=1662118076"
             alt=""
           />
-          <div className="p-5 text-white">
-            <p className="text-gray-500">T-shirt</p>
+          <div className="shop-text">
             <h1>Harry Style</h1>
-            <p className="py-2">Love On Tour Bunny TV Longsleeve - White</p>
-            <p>60.00 $</p>
-            <div className="pt-2 text-center">
-              <button className="w-full uppercase text-sm font-bold py-3 px-4 text-gray-200 rounded-md bg-gradient-to-r from-purple-600 via-violet-700 to-purple-600 active:scale-95">
-                Buy now
-              </button>
-            </div>
+            <p>Love On Tour Bunny TV Longsleeve - White</p>
+          </div>
+          <div className="shop-price">
+            <p>$29.99 </p>
           </div>
         </div>
-        <div className="   rounded-lg shadow bg-gray-800">
+        <div className="shop-container">
           <img
-            className="rounded w-full object-cover"
+            className="shop-image"
             src="https://e.snmc.io/i/600/s/71735f0d244ec9c5261daedf23b1a85e/8993070/annalynn-a-conversation-with-evil-Cover-Art.jpg"
             alt=""
           />
-          <div className="p-5 text-white">
-            <p className="text-gray-500">Albums</p>
+          <div className="shop-text">
             <h1>Annalynn</h1>
-            <p className="py-2">Album: A Conversation With Evil</p>
-            <p>29.99 $</p>
-            <div className="pt-2 text-center">
-              <button className="w-full uppercase text-sm font-bold py-3 px-4 text-gray-200 rounded-md bg-gradient-to-r from-purple-600 via-violet-700 to-purple-600 active:scale-95">
-                Buy now
-              </button>
-            </div>
+            <p>Album: A Conversation With Evil</p>
+          </div>
+          <div className="shop-price">
+            <p>$29.99 </p>
           </div>
         </div>
-        <div className="   rounded-lg shadow bg-gray-800">
+        <div className="shop-container">
           <img
-            className="rounded w-full object-cover"
+            className="shop-image"
             src="https://ryzm.imgix.net/sites/1f46106e-0d50-4f79-8418-1f8e72db18c8/images/608451fa-5d67-452b-bd16-e54dcb963a82?fit=crop&fill=blur&w=800&h=800"
             alt=""
           />
-          <div className="p-5 text-white">
-            <p className="text-gray-500">T-shirt</p>
+          <div className="shop-text">
             <h1>Coldrain</h1>
-            <p className="py-2">RE:ADMISSION” 80's Crack T-Shirts</p>
-            <p>31.99 $</p>
-            <div className="pt-2 text-center">
-              <button className="w-full uppercase text-sm font-bold py-3 px-4 text-gray-200 rounded-md bg-gradient-to-r from-purple-600 via-violet-700 to-purple-600 active:scale-95">
-                Buy now
-              </button>
-            </div>
+            <p>RE:ADMISSION” 80's Crack T-Shirts</p>
+          </div>
+          <div className="shop-price">
+            <p>$29.99 </p>
           </div>
         </div>
-        <div className="   rounded-lg shadow bg-gray-800">
+        <div className="shop-container">
           <img
-            className="rounded w-full object-cover"
+            className="shop-image"
             src="https://shop-us.bmthofficial.com/cdn/shop/files/T-shirt.jpg?v=1686329205&width=1000"
             alt=""
           />
-          <div className="p-5 text-white">
-            <p className="text-gray-500">T-shirt</p>
+          <div className="shop-text">
             <h1>Bring me the horizon</h1>
-            <p className="py-2">Post Human:Next Gen Tee</p>
-            <p>30.00 $</p>
-            <div className="pt-2 text-center">
-              <button className="w-full uppercase text-sm font-bold py-3 px-4 text-gray-200 rounded-md bg-gradient-to-r from-purple-600 via-violet-700 to-purple-600 active:scale-95">
-                Buy now
-              </button>
-            </div>
+            <p>Post Human:Next Gen Tee</p>
+          </div>
+          <div className="shop-price">
+            <p>$29.99 </p>
           </div>
         </div>
       </div>
